5b8efa9a6610f6f1bdbb124430c1a290450d524b
Xinshuang Jin <jinxinshuang@gmail.com>
2025-07-22 16:19:54 -0400

    edited GET/listings to include images in the result

--- routes/listings.js
+++ routes/listings.js
@@ -14,8 +14,20 @@ const { pool } = require('../db/index');
 // });
 router.get('/', async (req, res) => {
   try {
-    const query = `SELECT * FROM listing`;
-    const result = await pool.query(query);
+    // const query = `SELECT * FROM listing`;
+    // const result = await pool.query(query);
+    const getUserListingsAndImagesQuery = `
+      SELECT 
+        l.*,
+        COALESCE(
+        json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
+        '[]'
+      ) AS images
+      FROM listing l
+      LEFT JOIN image i ON l.listing_id = i.listing_id
+      GROUP BY l.listing_id;
+    `;
+    const result = await pool.query(getUserListingsAndImagesQuery);
     res.status(200).json(result.rows);
   } catch (err) {
     console.error('Error fetching listings:', err.message);
