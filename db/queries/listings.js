15a5f3925a1cf61ae30543efff5fd1bb170532ce
Xinshuang Jin <jinxinshuang@gmail.com>
2025-07-24 12:57:38 -0400

    refactored listings routes, moved queries to db/queries/listings.js

---
+++ db/queries/listings.js
@@ -0,0 +1,51 @@
+module.exports = {
+  GET_ALL_LISTINGS_WITH_IMAGES: `
+    SELECT 
+    l.*, 
+    COALESCE(
+    json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
+    '[]'
+    ) AS images
+    FROM listing l
+    LEFT JOIN image i ON l.listing_id = i.listing_id
+    GROUP BY l.listing_id;
+    `,
+
+    // with comment
+    // SELECT 
+    // l.*,  -- Select all columns from the listing table
+    // COALESCE( -- It checks each value in order.Returns the first one that is not NULL.
+    // json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL), -- Aggregate related images into a JSON array, ignoring NULL image rows
+    // '[]' -- If no images exist, return an empty array instead of null
+    // ) AS images
+    // FROM listing l
+    // LEFT JOIN image i ON l.listing_id = i.listing_id -- Join images to listings using listing_id; keeps listings even if no images
+    // GROUP BY l.listing_id;   -- Required for aggregation; groups results by listing
+    // `,
+
+    // Testing wrong query
+    //   GET_ALL_LISTINGS_WITH_IMAGES: `
+    //     SELECT 
+    //     l.*,
+    //     COALESCE(
+    //     json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
+    //     '[]'
+    //     ) AS images
+    //     FROM listings l
+    //     LEFT JOIN image i ON l.listing_id = i.listing_id
+    //     GROUP BY l.listing_id;
+    //     `,
+
+  GET_LISTING_WITH_IMAGES_BY_ID: `
+    SELECT 
+    l.*,
+    COALESCE(
+    json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
+    '[]'
+    ) AS images
+    FROM listing l
+    LEFT JOIN image i ON l.listing_id = i.listing_id
+    WHERE l.listing_id = $1
+    GROUP BY l.listing_id;
+    `,
+};
--- routes/listings.js
+++ routes/listings.js
@@ -1,67 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-// const validateModelById = require('./routes-utilities');
 const { pool } = require('../db/index');
-const validateModelById = require('./routes-utilities');
-
+const { validateModelById } = require('./routes-utilities'); 
+const listingQueries = require('../db/queries/listings');
 
 // GET /listings
-// router.get('/', async (req, res) => {
-//   const query = `SELECT * FROM listing`;
-//   const result = await pool.query(query);
-
-//   res.status(200).json(result.rows);
-// });
 router.get('/', async (req, res) => {
   try {
-    // const query = `SELECT * FROM listing`;
-    // const result = await pool.query(query);
-    const getUserListingsAndImagesQuery = `
-      SELECT 
-        l.*,
-        COALESCE(
-        json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
-        '[]'
-      ) AS images
-      FROM listing l
-      LEFT JOIN image i ON l.listing_id = i.listing_id
-      GROUP BY l.listing_id;
-    `;
-    const result = await pool.query(getUserListingsAndImagesQuery);
+    // const getUserListingsAndImagesQuery = `
+    //   SELECT 
+    //     l.*,
+    //     COALESCE(
+    //     json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
+    //     '[]'
+    //   ) AS images
+    //   FROM listing l
+    //   LEFT JOIN image i ON l.listing_id = i.listing_id
+    //   GROUP BY l.listing_id;
+    // `;
+
+    // const result = await pool.query(getUserListingsAndImagesQuery);
+
+    const query = listingQueries.GET_ALL_LISTINGS_WITH_IMAGES; 
+    const result = await pool.query(query);
     res.status(200).json(result.rows);
   } catch (err) {
-    console.error('Error fetching listings:', err.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error('Error fetching listings:', err); 
+    res.status(500).json({ error: err.message || 'Internal server error' }); 
   }
 });
 
 // GET /listings/<listing_id>
 router.get('/:listingId', async(req, res)=> {
-
   const listingId = req.params.listingId
 
   try {
-
     await validateModelById('listing', listingId)
 
-    const getSpecificListingsAndImagesQuery = `
-      SELECT 
-        l.*,
-        COALESCE(
-        json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
-        '[]'
-      ) AS images
-      FROM listing l
-      LEFT JOIN image i ON l.listing_id = i.listing_id
-      WHERE l.listing_id = $1
-      GROUP BY l.listing_id;
-    `;
-    const result = await pool.query(getSpecificListingsAndImagesQuery, [listingId]);
+    // const getSpecificListingsAndImagesQuery = `
+    //   SELECT 
+    //     l.*,
+    //     COALESCE(
+    //     json_agg(i.*) FILTER (WHERE i.image_id IS NOT NULL),
+    //     '[]'
+    //   ) AS images
+    //   FROM listing l
+    //   LEFT JOIN image i ON l.listing_id = i.listing_id
+    //   WHERE l.listing_id = $1
+    //   GROUP BY l.listing_id;
+    // `;
+    // const result = await pool.query(getSpecificListingsAndImagesQuery, [listingId]);
+
+    const query = listingQueries.GET_LISTING_WITH_IMAGES_BY_ID
+    const result = await pool.query(query, [listingId]);
     res.status(200).json(result.rows[0]);
 
   } catch (err) {
-    res.status(err.statusCode || 500).json({ error: err.message });
+    console.error('Error fetching listings:', err); 
+    res.status(err.statusCode || 500).json({ error: err.message || 'Internal server error' }); 
   }
 });
 
